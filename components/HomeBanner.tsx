35b1c99b111f6d9833afe7fc783b143b68cf7aa9
Taranpreet Singh <ramghrialoveysingh123@gmail.com>
2025-08-07 23:18:54 +0530

    fix video issue

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -1,5 +1,35 @@
+"use client"
+import { useEffect, useRef } from "react"
+
+const isSafari = () => {
+    const ua = navigator.userAgent.toLowerCase();
+    return ua.indexOf("safari") > -1 && ua.indexOf("chrome") < 0;
+};
 
 export default function HomeBanner() {
+    const videoParentRef = useRef<HTMLDivElement>(null)
+    useEffect(() => {
+        if (isSafari() && videoParentRef.current) {
+            const player = videoParentRef.current.children[0] as HTMLVideoElement;
+            if (player) {
+                player.controls = false;
+                player.playsInline = true;
+                player.muted = true;
+                player.setAttribute("muted", "");
+                player.autoplay = true;
+                setTimeout(() => {
+                    const promise = player.play();
+                    if (promise.then) {
+                        promise
+                            .then(() => { })
+                            .catch(() => {
+                            });
+                    }
+                }, 0);
+            }
+        }
+    }, []);
+
     return <section className="w-full relative  h-full min-h-screen mb-20 flex items-center justify-center  overflow-hidden ">
         <div className="relative z-[3] w-full   h-fit bg-transparent">
             <h1 className="text-7xl pt-1  bg-clip-text bg-gradient-to-r from-purple-600 text-transparent via-white to-zinc-800 lg:text-9xl font-bold text-center  relative z-20">
@@ -12,16 +42,27 @@ export default function HomeBanner() {
             <div className="absolute inset-x-0 lg:inset-x-60 bottom-0 mx-auto bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
             <div className="absolute inset-x-0 lg:inset-x-60 bottom-0 mx-auto bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
         </div>
-        <div className="w-full h-full absolute  inset-0">
-            <video
+        <div
+            ref={videoParentRef}
+            dangerouslySetInnerHTML={{
+                __html: ` <video
             autoPlay
             muted
             playsInline
             loop 
             preload="auto"
-            className="w-full h-full object-cover">
+             style="
+            position: absolute;
+            inset: 0;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            z-index: 0;
+        ">
                 <source src="/video/smoke.mp4" type="video/mp4" />
-            </video>
+            </video>`}}
+            className="w-full h-full absolute  inset-0">
+
         </div>
     </section>
 }
