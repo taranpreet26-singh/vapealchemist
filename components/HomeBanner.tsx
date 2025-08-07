78aac33acfb70b1cbc73f4bbd885d8b148521138
Taranpreet Singh <ramghrialoveysingh123@gmail.com>
2025-08-07 20:30:52 +0530

    add style in button

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -1,8 +1,22 @@
 "use client"
-import {  useRef } from "react"
+import {  useEffect, useRef } from "react"
 
 export default function HomeBanner() {
     const ref = useRef<HTMLVideoElement>(null)
+
+    useEffect(()=>{
+        const video = ref.current
+        if(video){
+            const playPromise = video.play()
+            if(playPromise !== undefined){
+                playPromise.then(()=>{
+
+                }).catch(()=>{
+                    console.log("erro")
+                })
+            }
+        }
+    },[])
     return <section className="w-full relative  h-full min-h-screen mb-20 flex items-center justify-center  overflow-hidden ">
         <div className="relative z-[3] w-full   h-fit bg-transparent">
             <h1 className="text-7xl pt-1  bg-clip-text bg-gradient-to-r from-purple-600 text-transparent via-white to-zinc-800 lg:text-9xl font-bold text-center  relative z-20">
