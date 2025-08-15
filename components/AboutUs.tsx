d2f28db084ba331eed515d6e8ef72b83b11d0d1f
Taranpreet Singh <ramghrialoveysingh123@gmail.com>
2025-08-15 18:17:12 +0530

    fix images

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -8,7 +8,7 @@ export function AboutUs() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 py-20 lg:py-10 px-4 lg:px-20 gap-4  w-full">
       <WobbleCard
-        containerClassName="col-span-1 lg:col-span-2  h-full bg-pink-800 min-h-[fit] md:min-h-[500px] lg:min-h-[300px]"
+        containerClassName="col-span-1 lg:col-span-2  h-full bg-pink-800 min-h-[fit] min-h-[300px]"
         className=""
       >
         <div className="max-w-xs ">
@@ -24,7 +24,7 @@ export function AboutUs() {
           width={500}
           height={500}
           alt="linear demo image"
-          className="absolute hidden md:block -right-4 md:-right-[20%] lg:-right-[40%] grayscale filter md:-bottom-20 lg:-bottom-60 object-contain rounded-2xl"
+          className="absolute hidden md:block -right-4 md:-right-[20%] lg:-right-[30%] grayscale filter md:-bottom-20 lg:-bottom-60 object-contain rounded-2xl"
         />
       </WobbleCard>
       <WobbleCard containerClassName="col-span-1 min-h-[300px]">
@@ -35,7 +35,7 @@ export function AboutUs() {
         We offer curated flavors to suit your taste, with fair pricing and transparent service premium quality without compromise.
         </p>
       </WobbleCard>
-      <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-blue-900 min-h-[fit] md:min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
+      <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-blue-900 min-h-[fit] min-h-[300px]">
         <div className="max-w-sm">
           <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
             Your Experience, Our Priority
@@ -49,7 +49,7 @@ export function AboutUs() {
           width={500}
           height={500}
           alt="linear demo image"
-          className="absolute hidden md:block -right-10 md:-right-[20%] lg:-right-[20%] md:-bottom-30 lg:-bottom-40 object-contain rounded-2xl"
+          className="absolute hidden md:block -right-10 md:-right-[20%] lg:-right-[14%] md:-bottom-30 lg:-bottom-50 object-contain rounded-2xl"
         />
       </WobbleCard>
     </div>
